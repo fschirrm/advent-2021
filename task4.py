2aedd0f590e91220dbcddc60b69f4168c362a0f4
Frank <f.schirrmeister@t-online.de>
2021-12-04 10:40:32 +0100

    input task4git

---
+++ task4.py
@@ -0,0 +1,5 @@
+import utils.utils as utils
+
+num_list, bingo_list = utils.read_bingo("files/task4.txt")
+print(num_list)
+print(bingo_list)
--- utils/utils.py
+++ utils/utils.py
@@ -2,3 +2,19 @@ def read_file(f):
     with open(f) as f: 
         content = [line.strip().split(" ") for line in f.readlines()]
     return content
+
+def read_bingo(f):
+    with open(f) as f:
+        content=f.readlines()
+    #inp = myfile.readline().strip().split(",")
+    num_list = [int(str_num) for str_num in content[0].strip().split(",")]
+    bingo_list=[]
+    for i in range(1,len(content)):
+        line= content[i].strip()
+        if len(line) == 0:
+            curr_bingo = []
+            bingo_list.append(curr_bingo)
+        else:
+            curr_bingo.append([int(str_num) for str_num in line.split()])
+    if bingo_list[-1] == []: bingo_list.remove([])
+    return num_list, bingo_list
