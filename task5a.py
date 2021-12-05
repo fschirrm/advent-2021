0c2a913c9f85f2f3f12cdc1f383bfba4dc2f6805
Frank <f.schirrmeister@t-online.de>
2021-12-05 11:38:08 +0100

    Task 5

--- task5a.py
+++ task5a.py
@@ -6,16 +6,25 @@ grid=[[0 for x in range(dimension)] for y in range(dimension)]
 for i in range (0,len(c)):
     start_coord = [int(x) for x in c[i][0].split(',')]
     end_coord = [int(x) for x in c[i][2].split(',')]
-    if (start_coord[0] == end_coord[0]):
-        if start_coord[1]<end_coord[1]: s1=start_coord[1]; s2=end_coord[1]
-        else:s1=end_coord[1]; s2=start_coord[1]
-        for j in range (s1,s2+1):
-            grid[start_coord[0]][j] += 1
-    if (start_coord[1] == end_coord[1]):
-        if start_coord[0]<end_coord[0]: s1=start_coord[0]; s2=end_coord[0]
-        else:s1=end_coord[0]; s2=start_coord[0]
-        for j in range (s1,s2+1):
-            grid[j][start_coord[1]] += 1
+    num_points=0
+    if start_coord[0] < end_coord[0]: 
+        x_add=1; num_points=end_coord[0]-start_coord[0]+1
+    else:  
+        if start_coord[0] > end_coord[0]: 
+            x_add=-1; num_points=start_coord[0]-end_coord[0]+1
+        else: x_add=0
+    if start_coord[1] < end_coord[1]: 
+        y_add=1; num_points=end_coord[1]-start_coord[1]+1
+    else:  
+        if start_coord[1] > end_coord[1]: 
+            y_add=-1; num_points=start_coord[1]-end_coord[1]+1
+        else: y_add=0
+    curr_x=start_coord[0]; curr_y=start_coord[1]
+    for j in range(0,num_points):
+        grid[curr_x][curr_y]+=1
+        curr_x+=x_add; curr_y+=y_add 
+    #print(start_coord,'  ',end_coord,'  ',num_points )
+    #print(grid)
 count=0
 for i in range(0,dimension):
     for j in range (0,dimension):
---
+++ task5b.py
@@ -0,0 +1,28 @@
+import utils.utils as utils
+
+dimension=1000
+c = utils.read_file("files/task5.txt")
+#print(c)
+grid=[[0 for x in range(dimension)] for y in range(dimension)]
+# print(grid)
+for i in range (0,len(c)):
+    start_coord = [int(x) for x in c[i][0].split(',')]
+    end_coord = [int(x) for x in c[i][2].split(',')]
+    #print(start_coord,'   ', end_coord)
+    if (start_coord[0] == end_coord[0]):
+        if start_coord[1]<end_coord[1]: s1=start_coord[1]; s2=end_coord[1]
+        else:s1=end_coord[1]; s2=start_coord[1]
+        for j in range (s1,s2+1):
+            grid[start_coord[0]][j] += 1
+    if (start_coord[1] == end_coord[1]):
+        if start_coord[0]<end_coord[0]: s1=start_coord[0]; s2=end_coord[0]
+        else:s1=end_coord[0]; s2=start_coord[0]
+        for j in range (s1,s2+1):
+            grid[j][start_coord[1]] += 1
+#print(grid)
+count=0
+for i in range(0,dimension):
+    for j in range (0,dimension):
+        if grid[i][j] > 1: count+=1
+print (f"Number of overlapping points: {count}")
+    
