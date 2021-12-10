87221862faee99b75f601b1828c31b9498aa3945
Frank <f.schirrmeister@t-online.de>
2021-12-10 18:44:50 +0100

    task 8b

--- task8b.py
+++ task8b.py
@@ -1,7 +1,11 @@
 import utils.utils as utils
 
-c = utils.read_file("files/task8_test.txt")
-#print(c)
+def is_part(t_value, compare):
+    for i in range(len(t_value)):
+        if not (t_value[i] in compare): return(False)
+    return(True)
+
+c = utils.read_file("files/task8.txt")
 inp_list=[]
 for elem in c:
     delimiter = False
@@ -16,7 +20,7 @@ for elem in c:
             else:
                 num_list.append(''.join(l1))
     inp_list.append([num_list, puzzle_list])
-print(inp_list); print
+result_list=[]
 for elem in inp_list:
     digit_list=['' for i in range(10)]
     five_seg=[]; six_seg=[]
@@ -28,16 +32,23 @@ for elem in inp_list:
         if len(e1) ==5: five_seg.append(e1)
         if len(e1) ==6: six_seg.append(e1)
     for e2 in six_seg:
-        if (digit_list[4] in e2): 
+        if (is_part(digit_list[4],e2)): 
             digit_list[9]=e2
-        else: digit_list[0]=e2
+        else: 
+            if is_part(digit_list[7], e2): digit_list[0]=e2
+            else: digit_list[6]=e2
     for e2 in five_seg:
-        if digit_list[7] in e2: digit_list[3]=e2
+        if is_part(digit_list[7],e2): digit_list[3]=e2
         else:
-            if e2 in digit_list[9]: digit_list[5]=e2
-            else:
-                if len(set(e2)&set(digit_list[7]))==2: digit_list[2]=e2
-                else: digit_list[6]=e2
-    for i in range(len(digit_list)):
-        print (i,":   ",digit_list[i]); 
-    print()
+            if is_part(e2, digit_list[9]): digit_list[5]=e2
+            else: digit_list[2]=e2
+    curr_val=1000
+    result=0
+    for e3 in elem[1]:
+        for j in range(len(digit_list)):
+            if e3 == digit_list[j]:
+                result = result + curr_val * j
+                curr_val//=10
+                break
+    result_list.append(result)
+print(f"Result = {sum(result_list)}")
