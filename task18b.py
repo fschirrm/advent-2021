995b35665fee74ab7b551cac3a968f5abfc3636d
Frank <f.schirrmeister@t-online.de>
2021-12-21 18:52:23 +0100

    task 18

--- task18b.py
+++ task18b.py
@@ -48,46 +48,51 @@ for elem in inp_list:
         else: elem_new.append(int(num_str))
     task_list.append(elem_new)
 curr_result = task_list[0]
-for k in range (1,len(task_list)):
-    curr_result.insert(0,'[')
-    curr_result += task_list[k]
-    curr_result.append(']')
-    changed = True
-    while changed:
-        changed = False
-        level = 0
-        last_i = -1
-        for i in range (len(curr_result)):
-            if curr_result[i] == '[': level += 1
-            if curr_result[i] == ']': level -= 1
-            if isinstance(curr_result[i],int):
-                if (level == 5) and isinstance(curr_result[i+1],int) and  \
-                    curr_result[i+2] == ']':
-                    changed = True
-                    if last_i >= 0: curr_result[last_i] += curr_result[i]
-                    for j in range (i+3,len(curr_result)): 
-                        if isinstance(curr_result[j], int): 
-                            curr_result[j] += curr_result[i+1]
+max_mag = 0
+for k in range (len(task_list)):
+    for l in range(len(task_list)):
+        curr_result = task_list[k].copy()
+        if l!=k:
+            curr_result.insert(0,'[')
+            curr_result += task_list[l]
+            curr_result.append(']')
+            changed = True
+            while changed:
+                changed = False
+                level = 0
+                last_i = -1
+                for i in range (len(curr_result)):
+                    if curr_result[i] == '[': level += 1
+                    if curr_result[i] == ']': level -= 1
+                    if isinstance(curr_result[i],int):
+                        if (level == 5) and isinstance(curr_result[i+1],int) and  \
+                            curr_result[i+2] == ']':
+                            changed = True
+                            if last_i >= 0: curr_result[last_i] += curr_result[i]
+                            for j in range (i+3,len(curr_result)): 
+                                if isinstance(curr_result[j], int): 
+                                    curr_result[j] += curr_result[i+1]
+                                    break
+                            curr_result.pop(i-1); 
+                            curr_result.pop(i-1); 
+                            curr_result.pop(i-1); 
+                            curr_result.pop(i-1);  
+                            curr_result.insert(i-1,0)
                             break
-                    curr_result.pop(i-1); 
-                    curr_result.pop(i-1); 
-                    curr_result.pop(i-1); 
-                    curr_result.pop(i-1);  
-                    curr_result.insert(i-1,0)
-                    break
-                else: last_i = i
-        if not changed:
-            for i in range (len(curr_result)):
-                if isinstance(curr_result[i],int) and curr_result[i] > 9:
-                    n1 = curr_result[i] // 2
-                    if curr_result[i] % 2 > 0: n2 = n1 + 1
-                    else: n2 = n1
-                    curr_result.pop(i)
-                    curr_result.insert(i,'[')
-                    curr_result.insert(i+1,n1)
-                    curr_result.insert(i+2,n2)
-                    curr_result.insert(i+3,']')
-                    changed = True
-                    break
-print(curr_result)
-print(mag (curr_result))
+                        else: last_i = i
+                if not changed:
+                    for i in range (len(curr_result)):
+                        if isinstance(curr_result[i],int) and curr_result[i] > 9:
+                            n1 = curr_result[i] // 2
+                            if curr_result[i] % 2 > 0: n2 = n1 + 1
+                            else: n2 = n1
+                            curr_result.pop(i)
+                            curr_result.insert(i,'[')
+                            curr_result.insert(i+1,n1)
+                            curr_result.insert(i+2,n2)
+                            curr_result.insert(i+3,']')
+                            changed = True
+                            break
+        curr_mag = mag(curr_result)
+        if curr_mag > max_mag : max_mag = curr_mag
+print(max_mag)
