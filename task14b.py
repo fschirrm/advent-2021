a0efaed21d0217febb51c6a797d3f917840b2ada
Frank <f.schirrmeister@t-online.de>
2022-01-06 18:00:26 +0100

    task 14

--- task14b.py
+++ task14b.py
@@ -38,9 +38,9 @@ for i in range(40):
                 loop_dict_work[elem] += loop_dict[k]
     loop_dict = loop_dict_work.copy()
     for k in loop_dict_work: loop_dict_work[k] = 0
-    max = -1; min = -1
-    max_key = ''; min_key = ''
 
+max = -1; min = -1
+max_key = ''; min_key = ''
 for k in frequ_dict:
     if frequ_dict[k] > max:
         max = frequ_dict[k]
@@ -48,7 +48,7 @@ for k in frequ_dict:
     if (frequ_dict[k] < min) or (min == -1):
         min = frequ_dict[k]
         min_key = k
-        
+
 print (f"Min: {min_key}={min}")
 print (f"Max: {max_key}={max}")
 print (f"Result: {max - min}")
