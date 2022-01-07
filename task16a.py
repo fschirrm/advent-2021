bb701d53aea356ade77d582f1d37102eeaddbf5b
Frank <f.schirrmeister@t-online.de>
2022-01-07 14:22:16 +0100

    task 16

--- task16a.py
+++ task16a.py
@@ -6,15 +6,18 @@ def check_packet(packet_bin):
     t = int(packet_bin[3:6],2)
     ver_value += v
     if t == 4:
-        end_ind = len(packet_bin) - 1
+        num_point = 6
+        while packet_bin[num_point:num_point+1] == "1": num_point += 5 
+        end_ind = num_point + 4
     else:
         if packet_bin[6:7] == '0':
             num_bits = int(packet_bin[7:22],2)
             end_ind = 0
-            while end_ind +1 < num_bits:
-                v, end_ind = check_packet(packet_bin[22+end_ind:22+num_bits])
+            while end_ind < num_bits:
+                v, e = check_packet(packet_bin[22+end_ind:22+num_bits])
+                end_ind = end_ind + e + 1
                 ver_value += v
-            end_ind = 22 + num_bits
+            end_ind = 21 + num_bits
         else:
             num_packets = int(packet_bin[7:18],2)
             start = 18
@@ -22,15 +25,14 @@ def check_packet(packet_bin):
                 v, end_ind = check_packet(packet_bin[start:])
                 ver_value += v
                 start = start + end_ind + 1
-            end_ind -= 1
+            end_ind = start - 1
     return ver_value, end_ind
 
 
-with open("files/task16_test2.txt") as f: packet_hex = f.readline().strip()
-# print(packet_hex)
+with open("files/task16.txt") as f: packet_hex = f.readline().strip()
 packet_bin = ""
 for i in range(len(packet_hex)):
     bin_str = "{0:04b}".format(int(packet_hex[i], 16))
     packet_bin += bin_str
 ver_value, end_ind = check_packet(packet_bin)
-print (ver_value); print(end_ind)
+print (f"Version-Summe: {ver_value}")
