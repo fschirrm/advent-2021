4a23518c89dea725f5d16df03e193d4c04888d82
Frank <f.schirrmeister@t-online.de>
2022-01-09 16:01:29 +0100

    task 19

--- task19a.py
+++ task19a.py
@@ -4,17 +4,18 @@ def find_beacon (s1, s2):
     permut_list = [[0,1,2],[1,0,2],[2,1,0],[0,2,1],[2,0,1],[1,2,0]]
     point_list=[]
     arrangement = []
+    orient = []
     found = False
     for permut in permut_list:
         for m in range(len(s1)):
             if s1[m][0]!=0 or s1[m][1]!=0 or s1[m][2]!=0:
                 for n in range(len(s2)):
-                    if abs(s1[m][0]) == abs(s2[n][0]) and abs(s1[m][1]) == abs(s2[n][1]) and \
-                        abs(s1[m][2]) == abs(s2[n][2]):
+                    if abs(s1[m][0]) == abs(s2[n][permut[0]]) and abs(s1[m][1]) == abs(s2[n][permut[1]]) and \
+                        abs(s1[m][2]) == abs(s2[n][permut[2]]):
                         point_list.append(m)
                         orient = []
                         for k in range(3):
-                            if s1[m][k] == s2[n][k]: orient.append(1)
+                            if s1[m][k] == s2[n][permut[k]]: orient.append(1)
                             else: orient.append(-1)
         if len(point_list) >= 11:
             arrangement = permut
@@ -25,6 +26,7 @@ def find_beacon (s1, s2):
 
 def compare_scanner(scanner1, scanner2):
     found = False
+    arrangement = []; orient = []; scanner_pos = []
     for i in range(len(scanner1)):
         point_list = []
         for j in range(len(scanner2)):
@@ -38,7 +40,20 @@ def compare_scanner(scanner1, scanner2):
             break
     return found, point_list, arrangement, orient, scanner_pos
 
-beacon_list = utils.read_beacon("files/task19_test.txt")
+def insert_points(s2, result_list, rel_pos, point_list, arrangement, orient):
+    for m in range(len(s2)):
+        point = []
+        for k in range(3):
+            point.append(rel_pos[k] + s2[m][0][arrangement[k]]*orient[k]) 
+        point_found=False
+        for elem in result_list:
+            if (elem[0] == point[0]) and (elem[1] == point[1]) and (elem[2] == point[2]):
+                point_found = True
+                break
+        if not point_found: result_list.append(point)
+    return result_list
+
+beacon_list = utils.read_beacon("files/task19.txt")
 for scanner in beacon_list:
     for beacon in scanner:
         rel_list = []
@@ -49,10 +64,31 @@ for scanner in beacon_list:
             rel_list.append([x,y,z])
         beacon.append(rel_list)
 #print(beacon_list[0][0])
-found, point_list, arrangement, orient, scanner_pos = compare_scanner(beacon_list[0], beacon_list[1])
-print(found); 
-print(arrangement)
-print(orient)
-print(scanner_pos)
+prep_list = [[0,[0,0,0],[0,1,2],[1,1,1]]]
+rem_list = [i for i in range(1,len(beacon_list))]
+result_list = []
+for elem in beacon_list[0]: result_list.append(elem[0]) 
+while (len(prep_list) > 0) and (len(rem_list) > 0):
+    curr_elem = prep_list.pop(0)
+    s1 = beacon_list[curr_elem[0]]
+    new_rem_list = rem_list.copy()
+    for elem in rem_list:
+        s2 = beacon_list[elem]
+        found, point_list, arrangement, orient, scanner_pos = compare_scanner(s1, s2)
+        if found:
+            print (f"Found in pair {curr_elem[0]} and {elem}" )
+            rel_pos = []
+            for k in range(3):
+                pos_elem = curr_elem[1][k] + scanner_pos[curr_elem[2][k]] * curr_elem[3][k]
+                rel_pos.append(pos_elem)
+            s0_arrangement = [arrangement[curr_elem[2][0]],arrangement[curr_elem[2][1]], arrangement[curr_elem[2][2]]]
+            s0_orient= [curr_elem[3][0]*orient[curr_elem[2][0]], curr_elem[3][1]*orient[curr_elem[2][1]], curr_elem[3][2]*orient[curr_elem[2][2]]]
+            prep_list.append([elem,rel_pos, s0_arrangement, s0_orient])
+            new_rem_list.remove(elem)
+            result_list = insert_points(s2, result_list, rel_pos, point_list, s0_arrangement, s0_orient)
+    rem_list = new_rem_list
 print()
-for elem in point_list: print(beacon_list[0][elem][0]); 
+print(f"Number Beacons: {len(result_list)}"); 
+#new_list=sorted(result_list,key=lambda p: p[0])
+
+#for elem in new_list: print(elem); 
